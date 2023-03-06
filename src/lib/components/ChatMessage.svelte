ca57c95870a348f2c4fd98475809a27aef324fa7
Ulaş Saçlı <ulassacli@outlook.com>
2023-03-06 03:52:26 +0300

    sveltekit-chatgpt 3.5-turbo integration done.
    sveltekit-chatgpt 3.5-turbo integration done, i just forked Huntabyte's svelte-chatbot app repo which i found him from his youtube channel, and will be doing some changes in the integration code while creating my app on top of this chat functionality

---
+++ src/lib/components/ChatMessage.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { ChatCompletionRequestMessageRoleEnum } from 'openai'
+	export let type: ChatCompletionRequestMessageRoleEnum
+	export let message: string
+</script>
+
+<div class="chat {type === 'user' ? 'chat-end' : 'chat-start'} justify-end">
+	<div class="chat-image avatar">
+		<div class="w-10 rounded-full">
+			<img
+				src="https://ui-avatars.com/api/?name={type === 'user' ? 'Me' : 'B'}"
+				alt="{type} avatar"
+			/>
+		</div>
+	</div>
+	<div class="chat-header">
+		{type === 'user' ? 'Me' : 'Bot'}
+	</div>
+	<div class="chat-bubble {type === 'user' ? 'chat-bubble-primary' : 'chat-bubble-secondary'}">
+		{message}
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,81 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<script lang="ts">
+import type {ChatCompletionRequestMessage} from "openai"
+import ChatMessage from '$lib/components/ChatMessage.svelte'
+import {SSE} from "sse.js"
+
+
+
+let query:string  = ""
+let answer: string = ""
+let loading: boolean=false
+let chatMessages: ChatCompletionRequestMessage[] = []
+
+const handleSubmit = async()=>{
+loading = true,
+chatMessages =[...chatMessages, {role:"user", content:query}]
+
+const eventSource=new SSE("/api/chat",{
+    headers:{
+        "Content-Type": "application/json"
+    },
+    payload: JSON.stringify({
+        messages:chatMessages
+    })
+})
+query = ""
+
+eventSource.addEventListener("error", handleError)
+eventSource.addEventListener("messages", (e) =>{
+    try{
+        loading = false
+        if(e.data==="[DONE]"){
+            chatMessages=[...chatMessages, {role:"assistant", content:answer}]
+            answer=""
+            return
+        }
+        const completionResponse = JSON.parse(e.data)
+        const [{delta}]=completionResponse.choices
+
+        if(delta.content){
+            answer=(answer ?? "" ) + delta.content
+        }
+    }catch (err){
+        handleError(err)
+    }
+}) 
+eventSource.stream()
+}
+
+function handleError<T>(err: T){
+    loading = false
+    query=""
+    answer=""
+    console.error(err)
+}
+
+
+
+</script>
+
+
+<div class="flex flex-col pt-4 w-full px-8 items-center gap-2">
+	<div>
+		<h1 class="text-2xl font-bold w-full text-center">Chatty</h1>
+		<p class="text-sm italic">Powered by gpt-3.5-turbo</p>
+	</div>
+	<div class="h-[500px] w-full bg-gray-900 rounded-md p-4 overflow-y-auto flex flex-col gap-4">
+		<div class="flex flex-col gap-2">
+			<ChatMessage type="assistant" message="Hi, I'm Axel, how can I help?" />
+			<ChatMessage type="user" message="Hi" />
+			<ChatMessage type="assistant" message="Do you need something??" />
+			<ChatMessage type="user" message="Woah lose the attitude" />
+		</div>
+		<div class="" />
+	</div>
+	<form class="flex w-full rounded-md gap-4 bg-gray-900 p-4" on:submit|preventDefault={()=>handleSubmit}>
+		<input type="text" class="input input-bordered w-full" bind:value={query}/>
+		<button type="submit" class="btn btn-accent"> Send </button>
+	</form>
+</div>
