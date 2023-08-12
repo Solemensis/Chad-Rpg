7ad131b1186adaa2eb479f3a9a63a8604096600a
Ulaş Saçlı <ulassacli@outlook.com>
2023-08-12 15:59:04 +0300

    curent unf bard-ai changed to another unf bard-ai
    curent unf bard-ai changed to another unf bard-ai

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -6,26 +6,32 @@ import type { RequestHandler } from './$types'
 import { getTokens } from '$lib/tokenizer'
 import { json } from '@sveltejs/kit'
 import type { Config } from '@sveltejs/adapter-vercel'
-import Bard from 'bard-ai'
+// import Bard from 'bard-ai'
+import { Bard } from 'googlebard'
 
 export const config: Config = {
 	runtime: 'edge'
 }
 
-let myBard = new Bard(BARD_KEY)
-// let myBard = new Bard({
-// 	'__Secure-1PSID': BARD_KEY,
-// 	'__Secure-1PSIDTS': BARD_ex_KEY
-// })
+// let myBard = new Bard(BARD_KEY)
+// // let myBard = new Bard({
+// // 	'__Secure-1PSID': BARD_KEY,
+// // 	'__Secure-1PSIDTS': BARD_ex_KEY
+// // })
 
-let myConversation = myBard.createChat()
+// let myConversation = myBard.createChat()
+
+let cookies = `__Secure-1PSID=${BARD_KEY}; __Secure-1PSIDTS=${BARD_ex_KEY}`
+let bot = new Bard(cookies)
+let conversationId = 'some_random_id'
 
 export const POST: RequestHandler = async ({ request }: any) => {
 	const requestBody = await request.json()
 
 	// console.log('chat:: ', myConversation)
 
-	const bardResponse: any = await myConversation.ask(requestBody.prompt)
+	let response = await bot.ask(requestBody.prompt, conversationId)
+	// const bardResponse: any = await myConversation.ask(requestBody.prompt)
 
 	// console.log(bardResponse)
 
@@ -34,7 +40,8 @@ export const POST: RequestHandler = async ({ request }: any) => {
 	// 		'Content-Type': 'application/json'
 	// 	}
 	// })
-	return json(bardResponse)
+	// return json(bardResponse)
+	return json(response)
 }
 
 // export async function POST({ request }) {
