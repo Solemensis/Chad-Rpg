f53ff17761d9ca92754de823a3fc3d208e967d51
Solemensis <ulassacli@outlook.com>
2026-02-18 15:26:04 +0300

    chore: remove local vercel artifacts and update ignore

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DyauzocT.js","_app/immutable/chunks/k4vayzEn.js","_app/immutable/chunks/CuDy-hXz.js","_app/immutable/chunks/DUev1ZMx.js","_app/immutable/chunks/CsYA_GUq.js"];
-export const stylesheets = [];
-export const fonts = [];
